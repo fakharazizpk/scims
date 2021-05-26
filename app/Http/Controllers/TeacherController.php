129858830a6055d65179a97d27be267fe363e0b7
refaqat88 <refaqatkhattak88@gmail.com>
2021-05-26 12:47:12 +0500

    teacher profile page view and query for profile show

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Session;
 
 class TeacherController extends Controller
 {
@@ -28,6 +30,20 @@ class TeacherController extends Controller
 
     public function TeacherProfile()
     {
+        //dd(session()->all());
+        //dd(Session::get('userData'));
+        $teacher = DB::table('employee_info')
+            ->leftjoin('employee_contact', 'employee_info.emp_cnt_Id', '=', 'employee_contact.emp_cnt_Id')
+            ->leftjoin('gender', 'employee_info.gnd_Id', '=', 'gender.gnd_Id')
+            ->leftjoin('nationality', 'employee_info.nation_Id', '=', 'nationality.nation_Id')
+            ->leftjoin('domicile', 'employee_info.dom_Id', '=', 'domicile.dom_Id')
+            ->leftjoin('cast', 'employee_info.cast_Id', '=', 'cast.cast_Id')
+            ->leftjoin('blood_group', 'employee_info.bg_Id', '=', 'blood_group.bg_Id')
+            ->leftjoin('religion', 'employee_info.relig_Id', '=', 'religion.relig_Id')
+            ->leftjoin('emergency_contact', 'employee_info.emer_cnt_Id', '=', 'emergency_contact.emer_cnt_Id')
+            ->leftjoin('employment_info', 'employee_info.empt_Id', '=', 'employment_info.empt_Id')
+            ->where('emp_Cnic', Session::get('userData')['username'])->first();
+        //dd($teacher);
         return view('teacher.profile');
     }
 }
