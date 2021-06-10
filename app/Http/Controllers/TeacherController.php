85e6235ba1e84fcd8e903f763a9f78b72ed185b3
fakharazizpk <fakhar.aziz.pk@gmail.com>
2021-06-10 01:03:38 -0700

    Update TeacherController.php
    import employe info model

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BloodGroup;
+use App\Models\Cast;
+use App\Models\Relationship;
+use App\Models\Religion;
 use Illuminate\Support\Facades\Session;
 use App\Models\User;
-use App\Models\EmloyeeInfo;
+use App\Models\Nationality;
+use App\Models\Gender;
+use App\Models\MaritalStatus;
+use App\Models\EmployeeInfo;
+use App\Models\EmergencyContact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Redirect,Response;
@@ -37,7 +45,7 @@ class TeacherController extends Controller
         //dd(Session::get('userData'));
         $teacher = DB::table('employee_info')
             ->leftjoin('employee_contact', 'employee_info.emp_cnt_Id', '=', 'employee_contact.emp_cnt_Id')
-            ->rightJoin('users', 'users.id', '=', 'employee_info.emp_id')
+            ->rightJoin('users', 'users.username', '=', 'employee_info.emp_Cnic')
             ->leftjoin('gender', 'employee_info.gnd_Id', '=', 'gender.gnd_Id')
             ->leftjoin('nationality', 'employee_info.nation_Id', '=', 'nationality.nation_Id')
             ->leftjoin('domicile', 'employee_info.dom_Id', '=', 'domicile.dom_Id')
@@ -52,6 +60,14 @@ class TeacherController extends Controller
     }
 
     public function editProfile(){
+        $nationalities= Nationality::all();
+        $gender = Gender::all();
+        $maritalstatus = MaritalStatus::all();
+        $bloodgroup = BloodGroup::all();
+        $relationship = Relationship::all();
+        $emergencyContact = EmergencyContact::all();
+        $cast = Cast::all();
+        $religion = Religion::all();
         $teacherprofile = DB::table('employee_info')
             ->leftjoin('employee_contact', 'employee_info.emp_cnt_Id', '=', 'employee_contact.emp_cnt_Id')
             ->leftjoin('gender', 'employee_info.gnd_Id', '=', 'gender.gnd_Id')
@@ -64,13 +80,14 @@ class TeacherController extends Controller
             ->leftjoin('emergency_contact', 'employee_info.emer_cnt_Id', '=', 'emergency_contact.emer_cnt_Id')
             ->leftjoin('employment_info', 'employee_info.empt_Id', '=', 'employment_info.empt_Id')
             ->where('emp_Cnic', Session::get('userData')['username'])->first();
-//    dd($teacherprofile);
-        return view('teacher.profile-edit',compact('teacherprofile'));
+//        dd($teacherprofile);
+        return view('teacher.profile-edit',compact('teacherprofile', 'gender', 'bloodgroup', 'nationalities', 'maritalstatus', 'relationship', 'cast', 'religion'));
     }
 
     public function ProfileUpdate(Request $request)
     {
 //        dd($request->all());
+
         $request->validate([
             'user_image' => 'image|mimes:jpeg,png,jpg,png|max:1024',
             'email' => 'required|email',
@@ -82,46 +99,49 @@ class TeacherController extends Controller
             'nationality' => 'required',
             'dob' => 'required',
             'religion' => 'required',
-            'domicile' => 'required',
+//            'domicile' => 'required',
             'cast' => 'required',
-            'city' => 'required',
-            'emer_cont_name' => 'required',
-            'emer_contact' => 'required',
-            'em_contact_rel' => 'required'
+//            'city' => 'required',
         ]);
 
 
-        $form_data1 = array(
+        $form_user = array(
 //            'name'      => $request->name,
             'email' => $request->email,
-            'phone' => $request->phone
+            'phone' => $request->phone,
         );
-        $form_data2 = array(
-            'fath_name'=> $request->fath_name,
+        $form_emp = array(
+            'emp_fat_Name'=> $request->fath_name,
             'emp_marital_Status'=> $request->marital_status,
-            'blood_group' => $request->blood_group,
-            'nationality' => $request->nationality,
+//            'emer_cnt_Id' => $request->emer_cont_name,
+            'bg_Id' => $request->blood_group,
+            'nation_id' => $request->nationality,
             'emp_Dob' => $request->dob,
-            'religion' => $request->religion,
-            'dom_District' => $request->domicile,
-            'cast' => $request->cast,
-            'emp_City' => $request->city,
-            'emer_cont_Name' => $request->emer_cont_name,
-            'emer_cont_No' => $request->emer_contact,
-            'fk_emer_relat_Id' => $request->em_contact_rel,
+            'relig_Id' => $request->religion,
+//            'dom_Id' => $request->domicile,
+            'cast_Id' => $request->cast,
+//            'city_Id' => $request->city,
             'gnd_Id' => $request->gender
 
         );
 
+        $form_emer_contact = array(
+            'emer_cont_Name' => $request->emer_cont_name,
+            'emer_cont_No' => $request->emer_contact,
+            'fk_emer_relat_Id' => $request->emer_contact_rel,
+        );
         if ($request->hasFile('user_image')) {
             $user_image = $request->file('user_image');
             $new_user_image = "user" . time() . '.' . $user_image->getClientOriginalExtension();
-            $user_image->move(public_path('upload/employee'), $new_user_image);
-            $form_data['user_image'] = $new_user_image;
+            $user_image->move(public_path('upload/user'), $new_user_image);
+            $form_user['user_image'] = $new_user_image;
         }
+//        dd($form_user);
 
-        User::where('id',Session::get('userData')['id'])->update($form_data1);
-//        Emloyee_info::where('id',Session::get('userData')['id'])->update($form_data2);
+        $user = User::where('id',Session::get('userData')['id'])->update($form_user);
+        EmployeeInfo::where('emp_Cnic',Session::get('userData')['username'])->update($form_emp);
+        EmergencyContact::where('emer_cnt_Id', $request->e_id)->update($form_emer_contact);
+
         $request->flash();
         return redirect()->back()->with('message', 'Successfully Updated!');
 
